3f562b60a1701926d4c9b15bd9173688d80d7b35
@coolio <test@MacBookPro.mshome.net>
2026-01-27 14:25:03 +0000

    [feat] ajout des schémas de migration

---
+++ database/migrations/2026_01_27_110822_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            
+            // ID généré pour CinetPay
+            $table->string('transaction_id', 100)->unique();
+
+            $table->string('customer_email', 255)->nullable();
+            $table->string('customer_phone', 50)->nullable();
+
+            $table->decimal('total_amount', 10, 2);
+
+            $table->enum('payment_status', ['pending', 'accepted', 'failed'])
+                  ->default('pending');
+
+            // Orange Money, Wave, etc.
+            $table->string('payment_method', 50)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
